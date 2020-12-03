9d5495f9d35bb13106e0fef366453c2bda144fc3
José Barreto <jbarretosantosjr@gmail.com>
2020-12-02 22:11:34 -0300

    initial commit

---
+++ test/esc_pos_bluetooth_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:esc_pos_brazilian/esc_pos_brazilian.dart';
+
+void main() {
+  test('Tests not implemented', () {
+    expect(1, 1);
+  });
+}
